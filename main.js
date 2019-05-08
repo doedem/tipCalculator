2c1264c99eedfe55791b71b4991718a198889c52
Harald Göthblad <haraldgothblad@Haralds-iMac.lan>
2019-05-08 14:28:23 +0200

    Started on calculator

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculateTip() {
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
+
+    
+}
